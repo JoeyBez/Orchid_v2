4c520e6f260b49cbb87428aa33ac30ad32233ae2
Joey Bezner <jmbez@comcast.net>
2026-08-30 21:58:10 -0400

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
+  const [currentSession, setCurrentSession] = useState();
+
+  useEffect(() => {
+    async function getSession(){
+      const { data: { session } } = await supabase.auth.getSession();
+      setCurrentSession(session);
+    }
+  }, []);
+
   return (
     <>
       
